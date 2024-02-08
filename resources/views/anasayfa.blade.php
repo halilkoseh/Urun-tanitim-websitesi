e3c5465114d94db02e8974b0d4d4c419583a0a9a
Halil Köse <kose.halil@std.izu.edu.tr>
2024-02-08 18:51:08 +0300

    fourth  commit

--- resources/views/anasayfa.blade.php
+++ resources/views/anasayfa.blade.php
@@ -9,6 +9,7 @@
 
 
 <title>Extra Sistem</title>
+<link rel="icon" href="images/unnamed-removebg-preview.png" type="image/x-icon">
 
 
 </head>
@@ -725,7 +726,8 @@ $(document).ready(function(){
 
 
 <div class="cookie-popup" id="cookiePopup">
-    <p>Sitemizde teknik olarak zorunlu olan çerezler kullanılmakta ve sizlere daha iyi bir hizmet sunabilmek için işlenmektedir. <br> Detaylı bilgi için <a href="https://www.kvkk.gov.tr/SharedFolderServer/CMSFiles/fb193dbb-b159-4221-8a7b-3addc083d33f.pdf" style="color:#ACADAF;">KVKK Aydınlatma Metni</a>'ni inceleyebilirsiniz.</p>
+    <p>Sitemizde teknik olarak zorunlu olan çerezler kullanılmakta ve sizlere daha iyi bir hizmet sunabilmek için işlenmektedir. <br>
+      Detaylı bilgi için <a href="https://www.kvkk.gov.tr/SharedFolderServer/CMSFiles/fb193dbb-b159-4221-8a7b-3addc083d33f.pdf" style="color:#ACADAF;">KVKK Aydınlatma Metni</a>'ni inceleyebilirsiniz.</p>
     <button class="cookie-btn" onclick="acceptCookies()">Anladım, kabul ediyorum</button>
 </div>
 <script>
