20880157986adfe3acf0518bb398653ba7f26d6e
palabont34 <palabont34@gmail.com>
2024-02-21 19:00:59 +0300

    son

--- resources/views/anasayfa.blade.php
+++ resources/views/anasayfa.blade.php
@@ -15,7 +15,6 @@
         @include('partials.contact')
         @include('partials.footer')
 
-asdasd
         <div class="cookie-popup" id="cookiePopup">
             <p>Sitemizde teknik     olarak zorunlu olan çerezler kullanılmakta ve sizlere daha iyi bir hizmet sunabilmek için
                işlenmektedir. <br>
